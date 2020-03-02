6c1b1ae22ad6c606c8d40047a2c9c1c5968bae0c
luisdev97 <amadormontorodavid@gmail.com>
2020-03-02 18:29:55 +0100

    Write complete list by web scraping

--- server.js
+++ server.js
@@ -1,44 +1,65 @@
-var fs = require('fs');
-var express = require("express");
+var express = require('express');
+const app = express();
+const osmosis = require('osmosis');
 var bodyParser = require("body-parser");
-var app = express();
+var fs = require('fs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+async function readJson(jsonPath, pokemon) {
+    let file = await fs.readFileSync(jsonPath,'utf-8');
+    let actualList = JSON.parse(file);
+    let updatedList = [...actualList, pokemon];
+    fs.writeFileSync('data.json', JSON.stringify(updatedList, 'utf-8'));
+}
 
 
-function readJson(jsonPath, pokemon){
-    
-     fs.readFile(jsonPath, 'utf-8', (err, data) => {
-        if(err){
-            console.log(err);
-            return err;
-        }
-
-        let actualList = JSON.parse(data);
-        console.log(actualList);
-        actualList.push(pokemon);
-        console.log('actualList');
-        let updatedList = JSON.stringify(actualList);
-        fs.writeFile('data.json', updatedList, 'utf-8', () => {});
-
-        
+async function scrapPokemon (id){
+    return new Promise((resolve, reject ) => {
+        let response =  [];
+        osmosis.get(`https://www.pokemon.com/es/pokedex/${id}`)
+        .find('p.version-y')
+        .set('pokemon')
+        .data(res => { response.push(res);})
+        .error(err => reject(err))
+        .done(() => resolve(response));
     });
 
 }
 
+app.get('/scrap', (req, res) => {
+    for(let i = 0; i < 890; i++){
+        
+            let id = i + 1;
+            scrapPokemon(id).then(res => {
+
+                
+                const pokemon = {
+                    id,
+                    description: res[0].pokemon
+                }
+                readJson('data.json', pokemon);
+            });
+
+    }
+    
+    res.json({
+        ok: true
+    });
+})
 
 
-app.post('/write', (req, res) => {
-    const pokemon = req.body.pokemon;
-    readJson('data.json', pokemon);
-   
-    res.send('ready');
+app.get('/readJson' , async(req, res) => {
+    let file = await fs.readFileSync('data.json','utf-8');
+    let lista = JSON.parse(file);
+    console.log(lista.find(i => i.id == 150));   
 });
 
-
-var server = app.listen(3000, function () {
-    console.log("app running on port.", server.address().port);
-});
-
+app.put('/sortJson', async(req, res) => {
+    let file = await fs.readFileSync('data.json','utf-8');
+    let lista = JSON.parse(file);
+})
+app.listen(3000, () => {
+    console.log('server run in port 3000');
+})
